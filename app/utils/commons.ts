d6542a7a666775040384ed39985ddb0e3ba39fc2
Federico Luzzi <fedeluzzi00@gmail.com>
2022-05-15 22:26:19 +0200

    chore: update subgraph endpoints

--- app/utils/commons.ts
+++ app/utils/commons.ts
@@ -2,9 +2,9 @@ import { ChainId } from "@swapr/sdk";
 
 export const SUBGRAPH_URL = {
   [ChainId.MAINNET]:
-    "https://api.thegraph.com/subgraphs/name/luzzif/swapr-mainnet-v2",
+    "https://api.thegraph.com/subgraphs/name/dxgraphs/swapr-mainnet-v2",
   [ChainId.XDAI]:
-    "https://api.thegraph.com/subgraphs/name/luzzif/swapr-xdai-v2",
+    "https://api.thegraph.com/subgraphs/name/dxgraphs/swapr-xdai-v2",
   [ChainId.ARBITRUM_ONE]:
-    "https://api.thegraph.com/subgraphs/name/luzzif/swapr-arbitrum-one-v3",
+    "https://api.thegraph.com/subgraphs/name/dxgraphs/swapr-arbitrum-one-v3",
 };
